47d5370bd41deb377bbef2182617feadb37d434e
qusai-alhanaktah <qusaialhanaktah@gmail.com>
2019-11-11 16:06:32 +0200

    project about me

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # About-me
+
+## This project is talking about me:
+* my education history. 
+* the job experience. 
+* the main goal i dream to reach it.
+
+**This project is built by Qusai-alhanaktah**
+
+*I got a little help from my instructor*
+
+## The tools which i have used:
+1. promot 
+2. alert
+3. switch
+4. comment command //
+5. consol
+
+## license
+This project is licensed under the MIT License.
+
+## Acknowledgements:
+1. copy the [reset.css](https://meyerweb.com/eric/tools/css/reset/)
+2. copy the [.eslintrc.json](https://github.com/LTUC/amman-201d2/blob/master/configs/.eslintrc.json)
---
+++ js/app.js
@@ -0,0 +1,108 @@
+'use strict'
+var user = prompt('type your name please')
+alert('welcom to my page: ' + user )
+
+
+
+var yourAnswer1 = prompt('is the sky with blue color?')
+var yourAnswer1 = yourAnswer1.toUpperCase();
+switch(yourAnswer1) {
+    case'YES':
+    alert('correct');
+    break;
+    case'Y':
+    alert('correct');
+    break;
+    case'NO':
+    alert('incorrect');
+    break;
+    case'N':
+    alert('incorrect');
+    break;
+    default :
+    alert('yor are wrong, this is yes/no qusetion, you have to answer with yes/no')
+    break;
+}
+//console.log('this is true')
+
+var yourAnswer2 = prompt('can the bird fly ?')
+var yourAnswer2 = yourAnswer2.toUpperCase();
+switch(yourAnswer2) {
+    case'YES':
+    alert('correct');
+    break;
+    case'Y':
+    alert('correct');
+    break;
+    case'NO':
+    alert('incorrect');
+    break;
+    case'N':
+    alert('incorrect');
+    break;
+    default :
+    alert('yor are wrong, this is yes/no qusetion, you have to answer with yes/no')
+    break;
+}
+var yourAnswer3 = prompt('the water is colorless?')
+var yourAnswer3 = yourAnswer3.toUpperCase();
+switch(yourAnswer3) {
+    case'YES':
+    alert('correct');
+    break;
+    case'Y':
+    alert('correct');
+    break;
+    case'NO':
+    alert('incorrect');
+    break;
+    case'N':
+    alert('incorrect');
+    break;
+    default :
+    alert('yor are wrong, this is yes/no qusetion, you have to answer with yes/no')
+    break;
+}
+var yourAnswer4 = prompt('is the earth sphericity?')
+var yourAnswer4 = yourAnswer4.toUpperCase();
+switch(yourAnswer4) {
+    case'YES':
+    alert('correct');
+    break;
+    case'Y':
+    alert('correct');
+    break;
+    case'NO':
+    alert('incorrect');
+    break;
+    case'N':
+    alert('incorrect');
+    break;
+    default :
+    alert('yor are wrong, this is yes/no qusetion, you have to answer with yes/no')
+    break;
+}
+var yourAnswer5 = prompt('is the sun shine in the night?')
+var yourAnswer5 = yourAnswer5.toUpperCase();
+switch(yourAnswer5) {
+    case'YES':
+    alert('incorrect');
+    break;
+    case'Y':
+    alert('incorrect');
+    break;
+    case'NO':
+    alert('correct');
+    break;
+    case'N':
+    alert('correct');
+    break;
+    default :
+    alert('yor are wrong, this is yes/no qusetion, you have to answer with yes/no')
+    break;
+}
+
+alert('welcom to my page again and i with you have a fun time with me: ' + user)
+
+
+
